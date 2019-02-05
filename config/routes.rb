eb7e59d52fff20d846478c0326a083b0ea8cc461
 NoraBourouis <bourouisnora@gmail.com>
2019-02-05 16:25:44 +0100

    lol

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   get'/', to: "static#welcome"
   post '/', to: 'static#home'
   get '/welcome/:first_name', to: 'static#welcome', as: 'welcome'
-  get '/gossips/:id/', to: 'static#gossips'
+
+  get '/gossips/:id/', to: 'gossip#gossips'
   get '/profils/:id/', to: 'static#profiles'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
