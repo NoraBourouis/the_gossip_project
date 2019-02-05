9b43fc32f780e2ef9eadd4061a0be88de7960c0a
 NoraBourouis <bourouisnora@gmail.com>
2019-02-05 17:44:38 +0100

    creation de critere pour class gossip

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -2,4 +2,8 @@ class Gossip < ApplicationRecord
   belongs_to :user
   has_many :tag_gossips
   has_many :tags, through: :tag_gossips
+  validates :title, presence: true, length: { in: 3..14 }
+  validates :content, presence: true
+end
+
 end
