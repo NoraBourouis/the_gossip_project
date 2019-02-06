7bd88585504cbb920fe8d8ad35c2ddd78bda4ab9
 NoraBourouis <bourouisnora@gmail.com>
2019-02-06 11:55:16 +0100

    tout à jour

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,18 +1,26 @@
 class GossipsController < ApplicationController
   def index
+   
   end
 
   def show
     # Méthode qui récupère le potin concerné et l'envoie à la view show (show.html.erb) pour affichage
   end
 
+  def new
+    @gossipnew = Gossip.new('title' => params[:title],
+      'content' => params[:content], 'user_id'=> rand(User.first.id..User.last.id))
+    # Méthode qui crée un potin vide et l'envoie une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
+  end
+
   def create
     puts params
-    @gossip = Gossip.new('title' => params[:title],
-                     'content' => params[:content])
-  
-    if @gossip.save
-      redirect_to show_path
+    @gossipnew = Gossip.new('title' => params[:title],
+                     'content' => params[:content],
+                     'user_id'=> rand(User.first.id..User.last.id))
+
+    if @gossipnew.save
+      redirect_to home_path
       puts "gossip created"
     else
       render 'new'
@@ -23,10 +31,6 @@ class GossipsController < ApplicationController
   end
 
 
-  def new
-    @gossip = Gossip.new('title' => params[:title],
-      'content' => params[:content])
-    # Méthode qui crée un potin vide et l'envoie une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
-  end
+
 
 end
